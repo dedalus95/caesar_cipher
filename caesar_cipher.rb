de3609fcad662c0f70e6e1aa374f784cd3722d44
Emanuele <ecerilli1@gmail.com>
2021-08-09 12:00:59 +0200

    done

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,13 +1,25 @@
 
 def caesar_cipher(string,n)
+  if n > 26
+    puts 'INVALID NUMBER'
+  return
+  end
   arr = string.split('')
-  p arr
-  arr.map! {|l| l.ord}
-  arr.map! {|num| num + n}
-  arr.map! {|num| num.chr}
+  arr.map! do
+    |l|      
+    if l.ord.between?(32,59)
+      result = l.ord + 0
+    else
+    result = l.ord + n 
+    end
+    if result > 122 || (l.ord.between?(65,90) && result > 90)
+      result =  result - 26
+    end
+    result.chr
+  end
   new_str = arr.join('')
-  puts new_str
+  p new_str
 end
 
-caesar_cipher('zebra', 1)
+caesar_cipher('YOU\'LL NEVER WALK ALONE', 54)
 
